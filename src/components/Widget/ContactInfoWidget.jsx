396ba00baadccbeca25e97a83947d0ec6541ac76
Muhammad Shoaib <wmfcagency@gmail.com>
2025-05-17 16:01:53 +0500

    Update ContactInfoWidget.jsx

--- src/components/Widget/ContactInfoWidget.jsx
+++ src/components/Widget/ContactInfoWidget.jsx
@@ -12,7 +12,7 @@ export default function ContactInfoWidget({withIcon, title}) {
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:envelope" /></span>:''}
-          shoaib@wmfcagency.com
+          wmfcagency@gmail.com
         </li>
         <li>
           {withIcon?<span className='cs-accent_color'><Icon icon="mdi:map-marker" /></span>:''}
